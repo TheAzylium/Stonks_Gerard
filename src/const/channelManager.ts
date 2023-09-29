63a00d8c4cd99b6be7d72ba25291d0b0673a48b2
TheAzylium <34239874+TheAzylium@users.noreply.github.com>
2023-09-29 12:44:45 +0200

    Remove EMoji and dot for channelName

--- src/const/channelManager.ts
+++ src/const/channelManager.ts
@@ -3,9 +3,13 @@ import slugify from 'slugify';
 export const channelMap = new Map<string, string>();
 
 export function formatChannelName(roleName: string): string {
-  return slugify(roleName, {
+  const noEmoji = roleName.replace(
+    /(?:\p{Emoji})(?!\p{Emoji_Modifier})|(\p{Emoji}\p{Emoji_Modifier}?)/gu,
+    '',
+  );
+  return slugify(noEmoji, {
     lower: true,
     replacement: '_',
-    remove: /[*+~.()'"!:@]/g,
+    remove: /[*+~.()'"!:@・]/g,
   });
 }
